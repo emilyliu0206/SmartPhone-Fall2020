f46838797df359c5b8381b9b27aa6d879abc889e
emilyliu0206 <liu.jiaxin2@husky.neu.edu>
2020-10-01 15:36:33 -0700

    add all images and basic frame work

---
+++ Assignment1/Assignment1/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Assignment1
+//
+//  Created by Jiaxin Liu on 10/1/20.
+//  Copyright © 2020 edu.northeastern.jiaxin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var player1: UILabel!
+    
+    @IBOutlet weak var player2: UILabel!
+    
+    @IBOutlet weak var player1card1: UIImageView!
+    
+    @IBOutlet weak var player1card2: UIImageView!
+    
+    @IBOutlet weak var player1card3: UIImageView!
+    
+    @IBOutlet weak var player2card1: UIImageView!
+    
+    @IBOutlet weak var player2card2: UIImageView!
+    
+    @IBOutlet weak var player2card3: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    func getCardsValue(){
+        
+    }
+    
+
+
+    @IBAction func playButton(_ sender: Any) {
+        
+    }
+}
+
